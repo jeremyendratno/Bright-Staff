0de0487df841def16a4b41640346af58970c60bf
Jeremy Endratno <jeremyendratno@gmail.com>
2022-01-03 16:26:05 +0700

    Initial Commit

---
+++ Bright Staff/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bright Staff
+//
+//  Created by Jeremy Endratno on 1/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
